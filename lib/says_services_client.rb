b581558cc62cac26bea074a23dfb5c929426278e
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2014-05-26 17:03:22 +0800

    Explicitly require files in the order that we want

--- lib/says_services_client.rb
+++ lib/says_services_client.rb
@@ -3,10 +3,19 @@ require "says_services_client/config"
 require 'active_model'
 require 'active_support/all'
 
-utils = File.join(File.dirname(__FILE__), "says_services_client", "utils", "*.rb")
-Dir.glob(utils).each {|file| require file}
-models = File.join(File.dirname(__FILE__), "says_services_client", "models", "*.rb")
-Dir.glob(models).each {|file| require file}
+require 'says_services_client/utils/class_level_inheritable_attributes'
+require 'says_services_client/utils/connection'
+require 'says_services_client/utils/operation_concerns'
+
+require 'says_services_client/models/base'
+require 'says_services_client/models/campaign'
+require 'says_services_client/models/campaign_list'
+require 'says_services_client/models/share'
+require 'says_services_client/models/share_list'
+require 'says_services_client/models/share_stat'
+require 'says_services_client/models/trigger'
+require 'says_services_client/models/user'
+require 'says_services_client/models/user_list'
 
 module SaysServicesClient
   class MissingEndpoint < StandardError
