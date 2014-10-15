584276eaeb50d3d11c6cfc7bd871c2d3b053a3fb
Soh Cher Wei <cherwei@Sohs-MacBook-Pro.local>
2014-10-15 17:39:48 +0800

    SaysServicesClient::RewardStore.update

--- lib/says_services_client/models/reward_store.rb
+++ lib/says_services_client/models/reward_store.rb
@@ -14,6 +14,15 @@ module SaysServicesClient
 
         super("/#{options[:country]}/api/v2/reward_stores/#{id}", params: options, service_name: :share_stat_url)
       end
+      
+      def update(id, options={})
+        options.symbolize_keys!
+        raise ActiveModel::MissingAttributeError.new("country") if options[:country].blank?
+        
+        request = establish_connection("/#{options[:country]}/api/v2/reward_stores/#{id}", service_name: :share_stat_url, method: :put, params: options)
+        response = request.run
+        parse!(response)
+      end
     end
   end
 end
