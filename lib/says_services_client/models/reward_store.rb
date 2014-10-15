ec8a18b1f0456d931f30c47cfa2e39c1d31e5ee1
Soh Cher Wei <cherwei@Sohs-MacBook-Pro.local>
2014-10-15 10:40:37 +0800

    SaysServicesClient::RewardStore.find

--- lib/says_services_client/models/reward_store.rb
+++ lib/says_services_client/models/reward_store.rb
@@ -7,6 +7,13 @@ module SaysServicesClient
 
         super("/#{options[:country]}/api/v2/reward_stores", params: options, service_name: :share_stat_url)
       end
+      
+      def find(id, options={})
+        options.symbolize_keys!
+        raise ActiveModel::MissingAttributeError.new("country") if options[:country].blank?
+
+        super("/#{options[:country]}/api/v2/reward_stores/#{id}", params: options, service_name: :share_stat_url)
+      end
     end
   end
 end
